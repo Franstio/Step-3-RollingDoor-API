d2204dbfb6b547fe6322b7ab92622e0006dfc733
FahriEfendi <fahriefendi0795@gmail.com>
2024-05-15 21:24:53 +0700

    Update Trigger Available Bin

--- Lib/PLCUtility.js
+++ Lib/PLCUtility.js
@@ -4,10 +4,18 @@ import ModbusRTU from 'modbus-serial';
 let client = null;
 export const getClient = () => {
     if (client == null) {
+        try
+        {
         client = new ModbusRTU();
         client.connectRTU("/dev/ttyUSB0", { baudRate: 9600 });
         //// set timeout, if slave did not reply back
         client.setTimeout(5000);
+        }
+        catch(error)
+        {
+            console.log(error);
+            return client;
+        }
     }
     return client;
 }
