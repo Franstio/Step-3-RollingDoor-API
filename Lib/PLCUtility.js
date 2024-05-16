966d0ce7f94ee9e3b3f2b398b45687c1984d3d68
FahriEfendi <fahriefendi0795@gmail.com>
2024-05-16 11:01:34 +0700

    Update Trigger Available Bin

--- Lib/PLCUtility.js
+++ Lib/PLCUtility.js
@@ -7,12 +7,13 @@ client.connectRTU("/dev/ttyUSB0", { baudRate: 9600 });
 //// set timeout, if slave did not reply back
 client.setTimeout(5000);
 export default client;
-export const switchLamp = async (lampType, isAlive) => {
+export const switchLamp = async (id,lampType, isAlive) => {
     const dict = {
         "RED": 30,
         "GREEN": 31
     };
     const address = dict[lampType];
+    client.setID(id);
     try
     {
         await client.writeRegister(address, isAlive ? 1 : 0);
