a9a9cca86e06d4c2c7afb3a7577b0dd3ce607915
FahriEfendi <fahriefendi0795@gmail.com>
2024-06-09 13:09:30 +0700

    add api manual.

--- controllers/TriggerRollingDoor.js
+++ controllers/TriggerRollingDoor.js
@@ -5,8 +5,6 @@ import bin from '../models/BinModel.js';
 //// set timeout, if slave did not reply back
 //client.setTimeout(5000);
 
-
-
 export const rollingdoorUp = async (req, res) => {
     try {
         const {idRollingDoor} = req.body;
@@ -77,12 +75,60 @@ export const rollingDoorDown = async (req, res) => {
     }
 };
 
-
-
-
 export const switchLampAPI = async (req,res) => {
     const {id,lamp,value} = req.query;
     console.log([id,lamp,value,value ? 1 : 0]);
     await switchLamp(id,lamp,value=='1');
     res.status(200).json({msg:"ok"});
 }
+
+
+export const rollingdoorUpManualWeb = async (req, res) => {
+    try {
+        const {idRollingDoor} = req.body;
+	console.log({id: idRollingDoor});
+
+       client.setID(idRollingDoor);
+        if (!client.isOpen) {
+            client.open( () => {
+                console.log("modbus open");
+           });
+        }
+        const address = 20;
+        const value = 1;
+        const log = await client.writeRegister(address,value);
+//        const data = await client.readHoldingRegisters(address, 8);
+//        console.log({ log: log, data: data });
+        if (value === 1) {
+            res.status(200).json({ msg: `Rolling Door Buka` });
+        } else {
+            res.status(200).json({ msg: `Kunci dengan address ${address} berhasil ditutup.` });
+        }
+
+
+    } catch (error) {
+        res.status(500).json({ msg: error });
+    }
+};
+
+export const rollingDoorDownManualWeb = async (req, res) => {
+    try {
+        const address = 21;
+        const value = 1;
+        const {idRollingDoor} = req.body;
+	console.log(idRollingDoor);
+
+        client.setID(idRollingDoor);
+//	await new Promise(resolve => setTimeout(resolve,5000));
+        await client.writeRegister(address, value);
+//        const data = await client.readHoldingRegisters(address, 8);
+
+        if (value === 1) {
+            res.status(200).json({ msg: `Rolling Door Ditutup ` + address + " " + value });
+        } else {
+            res.status(200).json({ msg: `Kunci dengan address ${address} berhasil ditutup.` });
+        }
+    } catch (error) {
+        res.status(500).json({ msg: error});
+    }
+};
--- routes/RollingDoorRoute.js
+++ routes/RollingDoorRoute.js
@@ -1,11 +1,13 @@
 import express from "express";
-import {rollingdoorUp,rollingDoorDown,triggerAvailableBin, switchLampAPI} from "../controllers/TriggerRollingDoor.js"
+import {rollingdoorUp,rollingDoorDown,triggerAvailableBin, switchLampAPI,rollingDoorDownManualWeb,rollingdoorUpManualWeb} from "../controllers/TriggerRollingDoor.js"
 
 const router = express.Router();
 
 
 router.post('/rollingdoorUp', rollingdoorUp);
 router.post('/rollingdoorDown',rollingDoorDown );
+router.post('/rollingdoorUpmanual', rollingdoorUpManualWeb);
+router.post('/rollingdoorDownmanual',rollingDoorDownManualWeb);
 router.post('/triggerAvailbleBin',triggerAvailableBin);
 router.get('/switchLamp',switchLampAPI);
 
