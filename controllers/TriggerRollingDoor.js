e78819f193fa7dcefd6362efa13d4e1c96099759
Franstio <franstiodiego01@gmail.com>
2024-07-06 14:31:40 +0700

    oK

--- controllers/TriggerRollingDoor.js
+++ controllers/TriggerRollingDoor.js
@@ -66,10 +66,10 @@ export const triggerAvailableBin = async (req,res) =>{
     }
     for (let i=0;i<availableBin.length;i++)
     {
-        console.log([parseFloat(availableBin[i].weight) , parseFloat(availableBin[i].max_weight),valueIsOpen,availableBin[i].clientId])
-        await switchLamp(availableBin[i].clientId, "RED",parseFloat(availableBin[i].weight) >= parseFloat(availableBin[i].max_weight) && valueIsOpen);
         await switchLamp(availableBin[i].clientId,"GREEN",parseFloat(availableBin[i].weight) < parseFloat(availableBin[i].max_weight) && valueIsOpen);
     }
+    for (let i= 0 ;i<availableBin.length;i++)
+        await switchLamp(availableBin[i].clientId, "RED",parseFloat(availableBin[i].weight) >= parseFloat(availableBin[i].max_weight) && valueIsOpen);
     res.status(200).json({msg:"Success Trigger bin"});
 }
 export const rollingDoorDown = async (req, res) => {
