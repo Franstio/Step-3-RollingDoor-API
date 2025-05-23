a3bda5ed57244a235472f6a89e5389a87e9447c6
admindpe-dharma <admindpe@dharmapacific.com>
2025-05-23 15:04:41 +0700

    Fix: Time login sales record

--- controllers/TriggerRollingDoor.js
+++ controllers/TriggerRollingDoor.js
@@ -180,7 +180,16 @@ export const Step4Check = async (binname)=>{
         const data = res.data.result;
         if (!data || data.length < 1)
             return false;
-        const lastDt = moment().format('YYYY-MM-DD HH:mm:ss');
+        let s = data[0].dt;
+        try
+        {
+            s = data[0].dt.replaceAll("T","").replaceAll("Z","");
+        }
+        catch (e)
+        {
+            s = data[0].dt;
+        }
+        const lastDt = moment(s).format('YYYY-MM-DD HH:mm:ss');
         const check = await db.query(`select * from bin where name='${binname}' and (last_empty < '${lastDt.toString()}' or last_empty is null);`,{
             type:QueryTypes.SELECT
         });
