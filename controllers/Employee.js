96f12a22c7b61b624cf6df63a5b85afa170b2642
Franstio <franstiodiego01@gmail.com>
2026-01-14 16:15:02 +0700

    ok

--- controllers/Employee.js
+++ controllers/Employee.js
@@ -45,9 +45,10 @@ export const ScanContainer = async (req, res) => {
         res.status(500).json({ msg: 'Terjadi kesalahan server' });
     }
 };
-export const CheckTransaction = async ()=>{
-    const check = await  db.query("select cast(recorddate as datetime) from transaction where cast(recordDate as datetime) >= now() - interval 1 minute order by id desc limit 0,1",{
-        type: QueryTypes.SELECT
+export const CheckTransaction = async (IdContainer)=>{
+    const check = await  db.query("select cast(recorddate as datetime) from transaction where idContainer=? and cast(recordDate as datetime) >= now() - interval 1 minute order by id desc limit 0,1",{
+        type: QueryTypes.SELECT,
+        replacements: [IdContainer]
     });
     return check.length > 0;
 }
@@ -59,12 +60,12 @@ export const SaveTransaksi = async (req,res) => {
     const tr =await db.transaction({isolationLevel: Transaction.ISOLATION_LEVELS.SERIALIZABLE});
     const state = [];
     const logoutdate = moment().format("YYYY-MM-DD HH:mm:ss");
-    if (await CheckTransaction())
-        return res.status(500).json({msg:"Transaction Cancelled (2)"});
     try
     {
         for (let i=0;i<payload.length;i++)
         {
+            if (await CheckTransaction(payload[i].idContainer))
+                return res.status(500).json({msg:"Transaction Cancelled (2)"});    
             payload[i].recordDate = logoutdate;
             payload[i].status = "PENDING|PIDSG|1";
             state.push(await transaction.create(payload[i],{transaction:tr}));
